fceca68a3325ae53d2d58e58895cae36d8f77a32
soda02 <richicsw@naver.com>
2017-06-22 17:38:10 +0900

    2017-06-22

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v*k!y-yoyqcz@y2+ozk&5ut36egcsd7b!w!eh9e2gug8uw!!1+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'soda02.pythonanywhere.com']
 
 
 # Application definition
